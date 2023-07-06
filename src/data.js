dbbf76a9c5dd3a469d0d08407fe4c4c6c046ced9
KeithPetr <91621041+KeithPetr@users.noreply.github.com>
2023-07-06 20:46:09 -0300

    added dog data and Dog class

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const dogs = [
+    {
+      name: "Rex",
+      avatar: "images/dog-rex.png",
+      age: 25,
+      bio: "Art. Literature. Natural wine. Yoga.",
+      hasBeenSwiped: false,
+      hasBeenLiked: false,
+    },
+    {
+      name: "Bella",
+      avatar: "images/dog-bella.jpg",
+      age: 43,
+      bio: "Life is a garden. Dig it!",
+      hasBeenSwiped: false,
+      hasBeenLiked: false,
+    },
+    {
+      name: "Teddy",
+      avatar: "images/dog-teddy.jpg",
+      age: 30,
+      bio: "I'd rather be at the beach!",
+      hasBeenSwiped: false,
+      hasBeenLiked: false,
+    },
+  ];
---
+++ src/dogs.js
@@ -0,0 +1,29 @@
+export class Dog {
+    constructor(data) {
+      Object.assign(this, data);
+    }
+  
+    getDogHtml() {
+      const { name, avatar, age, bio, hasBeenSwiped, hasBeenLiked } = this;
+      return `
+          <div class="img-container">
+              <img class="like-image none" id="like-image" src="images/like-image.png"/>
+              <img class="nope-image none" id="nope-image" src="images/nope-image.png"/>
+            <img class="dog-img" src="${avatar}" />
+            <p class="name-age">${name}, ${age}</p>
+            <p class="bio">${bio}</p>
+          </div>
+          `;
+    }
+  
+    setLiked() {
+      this.hasBeenSwiped = true;
+      this.hasBeenLiked = true;
+      document.getElementById('like-image').classList.remove('none')
+    }
+  
+    setNope() {
+      this.hasBeenSwiped = true;
+      document.getElementById('nope-image').classList.remove('none')
+    }
+  }
