05dea5d31fb399941e48aa045288f721e9f715e3
KeithPetr <keith.petryshyn@gmail.com>
2023-07-16 08:49:20 -0300

    profile-count now only displays when there is at least 1 liked profile

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,17 +1,17 @@
+/* eslint-disable react/prop-types */
 import ProfileIcon from "../images/profile-icon.png";
 import PawIcon from "../images/paw-icon.png";
 import ChatIcon from "../images/chat-icon.png";
-import LikedList from "./LikedList";
 
 export default function Header({getLikedList, likedDogs}) {
 
-  const display = likedDogs ? "block" : "none"
+  const display = likedDogs.length < 1 ? "none" : "flex"
 
   return (
     <header>
       <div onClick={getLikedList} className="profile-icon-container">
         <img className="profile-icon" src={ProfileIcon} />
-        <div className="profile-count" >{likedDogs.length}</div>
+        <div className="profile-count" style={{display: display}}>{likedDogs.length}</div>
       </div>
       <button type="button" className="paw-icon-container">
         <img className="paw-icon" src={PawIcon} />
